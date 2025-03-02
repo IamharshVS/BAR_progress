0b48aa9eb706ef45d421981d86f9d95f89592768
Harshvardhan Singh <harshvardhansingh1704@gmail.com>
2025-03-02 18:13:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let progress = 0;
+
+function increaseProgress() {
+    if (progress < 100) {
+        progress += 10;
+        const progressBar = document.getElementById("progressBar");
+        progressBar.style.width = progress + "%";
+        progressBar.innerText = progress + "%";
+    }
+}
